52599cca868baf773b0190a475a56fdf25b2abdc
Eric Vo <vo.eric@gmail.com>
2018-03-02 14:53:20 -0500

    merge

--- frontend/util/reservation_util.js
+++ frontend/util/reservation_util.js
@@ -2,13 +2,13 @@ export const maxOutClass = (db, thisClass, action) => {
   const numReservations = Object.keys(thisClass.reservations[thisClass.date]).length;
 
   if (thisClass.reservations && action === "hold") {
-    if (numReservations + 1 >= thisClass.seats) {
+      if (numReservations + 1 >= thisClass.seats) {
       db.ref('classes/' + thisClass.id + '/max').set(true);
     }
   } else if (thisClass.reservations) {
     db.ref('classes/' + thisClass.id + '/max').set(false);
   }
-}
+};
 
 export const confirmReserve = (db, thisClass, seats) => {
   const resId = db.ref("reservations").push().getKey();
@@ -18,10 +18,10 @@ export const confirmReserve = (db, thisClass, seats) => {
     date: thisClass.date,
     time: thisClass.time,
     created_at: new Date().getTime(),
-  })
+  });
   const ref = db.ref(`classes/${thisClass.id}/reservations/${thisClass.date}/${resId}`)
-    .set(true)
-}
+    .set(true);
+};
 
 export const hitReserve = reservation => {
   return $.ajax({
