b4f9c902adc33bd269d83c4a640a325150823d57
Thomas <thomasmaher1210@gmail.com>
2018-03-12 13:29:09 -0400

    Test log

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,6 +2,7 @@ class CardsController < ApplicationController
 
   def create
     Stripe.api_key = ENV['STRIPE_SK']
+    print ENV['STRIPE_SK']
 
     print params[:name], params[:token]
     customer = Stripe::Customer.create(
