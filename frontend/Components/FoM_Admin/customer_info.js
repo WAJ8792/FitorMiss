7acb3b848d5d630e97281164c544a868bbe0fd14
Thomas <thomasmaher1210@gmail.com>
2018-03-27 13:36:59 -0400

    Adds time and dividing line for new customer info in metrics

--- frontend/Components/FoM_Admin/customer_info.js
+++ frontend/Components/FoM_Admin/customer_info.js
@@ -5,11 +5,14 @@ import React from 'react';
 export default class extends React.Component {
   render() {
     const customer = this.props.customer;
+    const time = new Date(customer.created_at);
     return(
       <div id="mgmt-customer-info">
           <p>Email: {customer.email}</p>
           <p>Name: {customer.first_name + ' ' + customer.last_name}</p>
           <p>Neighborhood: {customer.neighborhood_id}</p>
+          <p>Time: {time.getHours()}:{time.getMinutes()}</p>
+          <hr />
       </div>
     )
   }
