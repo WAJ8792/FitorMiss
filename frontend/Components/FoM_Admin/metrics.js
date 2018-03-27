536f1485dc0df2172f8a6fd998d758fc50552048
Thomas <thomasmaher1210@gmail.com>
2018-03-26 21:27:22 -0400

    Adds created_at value to Customers

--- frontend/Components/FoM_Admin/metrics.js
+++ frontend/Components/FoM_Admin/metrics.js
@@ -1,27 +1,34 @@
 import React from 'react';
 
 export default class Metrics extends React.Component {
+  
+  customersToday() {
+    firebase.database().ref('customers')
+  }
+  
   render() {
     return (
       <div className="metrics">
         <h3>Metrics</h3>
         
-        <div>
-          <p>new customers today</p>
-        </div>
+        <section>
+          <div>
+            <p>new customers today</p>
+          </div>
+          
+          <div>
+            <p>classes booked today</p>
+          </div>
+          
+          <div>
+            <p>Visits to site today</p>
+          </div>
+          
+          <div>
+            <p>Current total customers</p>
+          </div>
         
-        <div>
-          <p>classes booked today</p>
-        </div>
-        
-        <div>
-          <p>Visits to site today</p>
-        </div>
-        
-        <div>
-          <p>Current total customers</p>
-        </div>
-        
+        </section>
       </div>
     )
   }
