924ab5280f6051c76b9f8a5dc38b86d17358d1c5
Thomas <thomasmaher1210@gmail.com>
2018-04-04 16:03:03 -0400

    Adds bkpilates site id

---
+++ frontend/Components/Customers/Accounts/ChangeCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class ChangeCard extends React.Component {
+  render() {
+    const props = this.props;
+    return(
+      <form onSubmit={e => props.changeCard(e)} id="change-card-form">
+        <p id="billing-header">Saved Cards</p>
+        <select
+          value={props.stripe_id}
+          onChange={props.selectCard}
+          style={{WebkitAppearance: 'menulist-button', width: '138px'}}>
+          {Object.keys(props.cards).map(stripe_id => {
+            if (stripe_id === props.stripe_id) {
+              return <option
+                value={stripe_id}
+                selected>
+                Card ending in {props.cards[stripe_id]}
+              </option>
+            } else {
+              return <option
+                value={stripe_id}>
+                Card ending in {props.cards[stripe_id]}
+              </option>
+            }
+          }
+          )}
+        </select>
+        {props.changeCardButton}
+      </form>
+    )
+  }
+}
